31f270a06b4ea372aa361135dc995d8fb54efc59
Yuri Marques <yuriifap@gmail.com>
2026-07-14 17:15:33 -0300

    feat: adiciona import {ListaProdutos}

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //Não vou usar routeroutlet agora!
+//substitui "imports:" RouterOutlet por Produto
 import { Produto } from './components/produto/produto';
+import {ListaProdutos} from './components/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
