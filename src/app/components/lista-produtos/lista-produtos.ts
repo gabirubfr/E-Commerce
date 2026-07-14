4a159c696af8f770d0e1626ed3bbde691936f008
Yuri Marques <yuriifap@gmail.com>
2026-07-14 17:24:28 -0300

    feat: adiciona função PrecoFormatadoPipe para mostrar produto e preço formatados

--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import{ Produto } from '../produto/produto'
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [Produto],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+export class ListaProdutos {
+  produtos = [
+    {nome: 'Teclado', preco: 49.99},
+    {nome: 'Mouse', preco: 29.99},
+    {nome: 'Monitor', preco: 599.99},
+    {nome: 'Desktop', preco: 49.99},
+    {nome: 'Headset', preco: 299.99}
+  ];
+}
