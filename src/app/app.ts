d131d39bc34c8886f6afc75498869cb5635dac24
Yuri Marques <yuriifap@gmail.com>
2026-07-16 13:50:32 -0300

    feat: cria imports listaProdutos e move para features

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //Não vou usar routeroutlet agora!
 //substitui "imports:" RouterOutlet por Produto
-import { Produto } from './components/produto/produto';
-import {ListaProdutos} from './components/lista-produtos/lista-produtos';
+import { Produto } from './features/produtos/produto/produto';
+import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
