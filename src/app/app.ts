9aca4c711ace59fc542e100ba60f560486c05161
Yuri Marques <yuriifap@gmail.com>
2026-07-22 16:12:26 -0300

    feat: adiciona funções de usuarioLogado, login e logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 //import { Produto } from './features/produtos/produto/produto';
 //import {ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { usuarioLogado, login, logout } from './core/auth';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
@@ -11,4 +12,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'Feira do Buritizal'; //nome do e-commerce
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
