8ee53f2f93471e5d71b5f1aee226c8ed3b63c363
Yuri Marques <yuriifap@gmail.com>
2026-07-22 16:09:09 -0300

    feat: import de authGuard e const routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,8 +26,11 @@
 //     },
 // ];
 
-//! código final LazyLoading e LoadComponent
+//! Importações
+
 import { Routes } from "@angular/router";
+import { authGuard } from "./core/auth.guard";
+
 export const routes: Routes = [
     {
         path: '',
@@ -41,6 +44,7 @@ export const routes: Routes = [
     },
     {
         path: 'carrinho',
+        canActivate: [authGuard],
      loadComponent: () =>
          import ('./features/carrinho/carrinho/carrinho').then((m) => m.Carrinho),
     },
